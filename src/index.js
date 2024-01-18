476c720e244c6760e346cad6c3a33fb58c688145
sumit yadav <sumit.yadav@codinova.com>
2024-01-18 15:58:49 +0530

    create a custom skeleton loader

---
+++ src/index.js
@@ -0,0 +1,25 @@
+// CustomLoader.js
+
+import './css/style.css'
+
+const CustomLoader = ({ height, width, pt, pr, pb, pl, mt, mr, mb, ml }) => {
+    const customstyled = {
+      width: width,
+      height: height,
+      paddingTop: pt,
+      paddingRight: pr,
+      paddingBottom: pb,
+      paddingLeft: pl,
+      marginTop: mt,
+      marginRight: mr,
+      marginBottom: mb,
+      marginLeft: ml,
+      display: 'block',
+      borderRadius: '5px',
+    };
+  
+    return <div className="custom-loader-css" style={customstyled}></div>;
+  };
+  
+  export default CustomLoader;
+  
