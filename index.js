10c582ba96b7ea33371a0e45460a221420a50a70
sumit yadav <sumit.yadav@codinova.com>
2024-01-20 15:43:16 +0530

    fix issue

---
+++ index.js
@@ -0,0 +1,1 @@
+export { default as CustomLoader } from './src/CustomLoader'
---
+++ src/CustomLoader/CustomLoader.jsx
@@ -0,0 +1,26 @@
+// CustomLoader.js
+import React from 'react';
+import 'CustomLoader.css'
+
+
+const CustomLoader = ({ height, width, pt, pr, pb, pl, mt, mr, mb, ml }) => {
+    const customstyled = {
+      width: width,
+      height: height,
+      paddingTop: pt,
+      paddingRight: pr,
+      paddingBottom: pb,
+      paddingLeft: pl,
+      marginTop: mt,
+      marginRight: mr,
+      marginBottom: mb,
+      marginLeft: ml,
+      display: 'block',
+      borderRadius: '5px',
+    };
+  
+    return <div className="custom-loader-css" style={customstyled}></div>;
+  };
+  
+  export default CustomLoader;
+  
